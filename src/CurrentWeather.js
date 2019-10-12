063c125ca375db437341d1a0abb3d5d158db559b
sarahlevins <sarahjeanlevins@gmail.com>
2019-10-12 10:35:42 +0800

    set up

---
+++ src/CurrentWeather.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CurrentWeather = () => {
+    return (
+        <div>
+            <div>20</div>
+            <div>Stockholm</div>
+        </div>
+    );
+};
+
+export default CurrentWeather;
